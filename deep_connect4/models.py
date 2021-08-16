87e96359421196c4e971ef93a202abff592e8238
Luca Arnaboldi <luca@arnaboldi.lu>
2021-08-16 16:14:39 +0200

    Reorganized Reinforcement Models

--- deep_connect4/models.py
+++ deep_connect4/models.py
@@ -32,3 +32,37 @@ class ConvolutionalNeuralNetwork(Model):
         x = self.hidden1(x)
         x = self.hidden2(x)
         return self.outlayer(x)
+
+class ScoreConvolutionalNeuralNetwork(Model):
+    def __init__(self):
+        super(ScoreConvolutionalNeuralNetwork, self).__init__()
+        self.conv = Conv2D(100, kernel_size = 4, input_shape=(N_ROW, N_COL, 1), activation = 'relu')
+        self.flatten = Flatten()
+        self.hidden1 = Dense(70, activation='relu')
+        self.hidden2 = Dense(70, activation='relu')
+        self.outlayer = Dense(1)
+
+    def call(self, x):
+        x = tf.expand_dims(x, 3) # I need to had a channel dimemension to use Conv2D layer
+        x = tf.cast(x, tf.float32) # I need to convert to an allowed dtype for convolutional network
+        x = self.conv(x)
+        x = self.flatten(x)
+        x = self.hidden1(x)
+        x = self.hidden2(x)
+        return self.outlayer(x)
+
+class ScoreNeuralNetwork(Model):
+    def __init__(self):
+        super(ScoreNeuralNetwork, self).__init__()
+        self.flatten = Flatten(input_shape=(N_ROW, N_COL))
+        self.hidden1 = Dense(100, activation='relu')
+        self.hidden2 = Dense(70, activation='relu')
+        self.hidden3 = Dense(70, activation='relu')
+        self.outlayer = Dense(1)
+
+    def call(self, x):
+        x = self.flatten(x)
+        x = self.hidden1(x)
+        x = self.hidden2(x)
+        x = self.hidden3(x)
+        return self.outlayer(x)
--- deep_connect4/reinforcement.py
+++ deep_connect4/reinforcement.py
@@ -26,40 +26,6 @@ class EpsilonGreadyPlayer(NeuralNetwrokScorePlayer):
         else:
             return super().move(board, moves, self_player)
 
-class ScoreConvolutionalNeuralNetwork(Model):
-    def __init__(self):
-        super(ScoreConvolutionalNeuralNetwork, self).__init__()
-        self.conv = Conv2D(100, kernel_size = 4, input_shape=(N_ROW, N_COL, 1), activation = 'relu')
-        self.flatten = Flatten()
-        self.hidden1 = Dense(70, activation='relu')
-        self.hidden2 = Dense(70, activation='relu')
-        self.outlayer = Dense(1)
-
-    def call(self, x):
-        x = tf.expand_dims(x, 3) # I need to had a channel dimemension to use Conv2D layer
-        x = tf.cast(x, tf.float32) # I need to convert to an allowed dtyper for convolutional network
-        x = self.conv(x)
-        x = self.flatten(x)
-        x = self.hidden1(x)
-        x = self.hidden2(x)
-        return self.outlayer(x)
-
-class ScoreNeuralNetwork(Model):
-    def __init__(self):
-        super(ScoreNeuralNetwork, self).__init__()
-        self.flatten = Flatten(input_shape=(N_ROW, N_COL))
-        self.hidden1 = Dense(100, activation='relu')
-        self.hidden2 = Dense(70, activation='relu')
-        self.hidden3 = Dense(70, activation='relu')
-        self.outlayer = Dense(1)
-
-    def call(self, x):
-        x = self.flatten(x)
-        x = self.hidden1(x)
-        x = self.hidden2(x)
-        x = self.hidden3(x)
-        return self.outlayer(x)
-
 class RLNeuralNetworkTrainer():
     def __init__(self, neural_network_model, n_players, model_name):
         self.models = [neural_network_model() for i in range(n_players)]
