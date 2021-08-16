d1864bdffed0d5e7aaa3df1d2002c0d01ffe2b74
Luca Arnaboldi <luca@arnaboldi.lu>
2021-08-16 11:28:44 +0200

    Reduced the dimension of the RL networks

--- deep_connect4/reinforcement.py
+++ deep_connect4/reinforcement.py
@@ -26,14 +26,13 @@ class EpsilonGreadyPlayer(NeuralNetwrokScorePlayer):
         else:
             return super().move(board, moves, self_player)
 
-class RLConvNeuralNetwork(Model):
+class ScoreConvolutionalNeuralNetwork(Model):
     def __init__(self):
         super(RLConvNeuralNetwork, self).__init__()
         self.conv = Conv2D(100, kernel_size = 4, input_shape=(N_ROW, N_COL, 1), activation = 'relu')
         self.flatten = Flatten()
         self.hidden1 = Dense(70, activation='relu')
-        self.hidden2 = Dense(50, activation='relu')
-        self.hidden3 = Dense(50, activation='relu')
+        self.hidden2 = Dense(70, activation='relu')
         self.outlayer = Dense(1)
 
     def call(self, x):
@@ -43,41 +42,18 @@ class RLConvNeuralNetwork(Model):
         x = self.flatten(x)
         x = self.hidden1(x)
         x = self.hidden2(x)
-        x = self.hidden3(x)
         return self.outlayer(x)
 
-class RLNeuralNetwork(Model):
+class ScoreNeuralNetwork(Model):
     def __init__(self):
         super(RLNeuralNetwork, self).__init__()
         self.flatten = Flatten(input_shape=(N_ROW, N_COL))
         self.hidden1 = Dense(100, activation='relu')
         self.hidden2 = Dense(70, activation='relu')
-        self.hidden3 = Dense(50, activation='relu')
-        self.hidden4 = Dense(50, activation='relu')
+        self.hidden3 = Dense(70, activation='relu')
         self.outlayer = Dense(1)
 
     def call(self, x):
-        x = self.flatten(x)
-        x = self.hidden1(x)
-        x = self.hidden2(x)
-        x = self.hidden3(x)
-        x = self.hidden4(x)
-        return self.outlayer(x)
-
-class RLConvNeuralNetwork(Model):
-    def __init__(self):
-        super(RLConvNeuralNetwork, self).__init__()
-        self.conv = Conv2D(100, kernel_size = 4, input_shape=(N_ROW, N_COL, 1), activation = 'relu')
-        self.flatten = Flatten()
-        self.hidden1 = Dense(70, activation='relu')
-        self.hidden2 = Dense(50, activation='relu')
-        self.hidden3 = Dense(50, activation='relu')
-        self.outlayer = Dense(1)
-
-    def call(self, x):
-        x = tf.expand_dims(x, 3) # I need to had a channel dimemension to use Conv2D layer
-        x = tf.cast(x, tf.float32) # I need to convert to an allowed dtyper for convolutional network
-        x = self.conv(x)
         x = self.flatten(x)
         x = self.hidden1(x)
         x = self.hidden2(x)
